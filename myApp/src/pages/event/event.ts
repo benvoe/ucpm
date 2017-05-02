548c97cc421a0eba4d8862b83b3df3ccbc5f83e0
Benedikt Völker <voelker.benedikt@yahoo.de>
2017-05-02 16:18:37 +0200

    beautify event list

--- myApp/src/pages/event/event.ts
+++ myApp/src/pages/event/event.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AddEventPage } from '../add_event/add_event';
+
 @Component({
   selector: 'page-event',
   templateUrl: 'event.html'
 })
 export class EventPage {
 
-  constructor(public navCtrl: NavController) {
+  pushPage: any;
 
+  constructor(public navCtrl: NavController) {
+    this.pushPage = AddEventPage;
   }
 
 }
