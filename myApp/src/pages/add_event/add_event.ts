89009f59a2f64a171dd3454c8db3bdc19043aa8a
Benedikt Völker <voelker.benedikt@yahoo.de>
2017-05-02 16:20:20 +0200

    create page to add or edit event

---
+++ myApp/src/pages/add_event/add_event.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-add-event',
+  templateUrl: 'add_event.html'
+  
+})
+export class AddEventPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
