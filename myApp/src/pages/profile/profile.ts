d7e067f149ea16f0d5e32a082a2f53c947d87327
Benedikt Völker <voelker.benedikt@yahoo.de>
2017-05-02 16:17:43 +0200

    Created profile page

--- myApp/src/pages/profile/profile.ts
+++ myApp/src/pages/profile/profile.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Storage } from '@ionic/storage';
+
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html'
 })
 export class ProfilePage {
+  
+  constructor(public navCtrl: NavController, public storage: Storage) {
+    
+    /*
+    this.storage.set('name','hans');
+    this.storage.get('name').then((val) => {
+      console.log(val);
+    })
+*/
+     storage.ready().then(() => {
 
-  constructor(public navCtrl: NavController) {
+       // set a key/value
+       storage.set('name', 'Max');
 
+       // Or to get a key/value pair
+       storage.get('name').then((val) => {
+         console.log('Your name is', val);
+       })
+     });
   }
+  
+  //private myName = "";
+  //private myName = String(this.storage.get('name'));
 
 }
