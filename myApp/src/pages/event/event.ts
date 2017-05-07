e4945c0ed097476f60795f3e5d785e8e15480f92
Benedikt Völker <voelker.benedikt@yahoo.de>
2017-05-07 11:37:25 +0200

    beautify events page for big screens

--- myApp/src/pages/event/event.ts
+++ myApp/src/pages/event/event.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 import { AddEventPage } from '../add_event/add_event';
+import { EventDetailPage } from '../eventdetails/eventdetails';
+import { EventData } from '../class/event_data';
+import { EventList } from '../class/event_list';
 
 @Component({
   selector: 'page-event',
@@ -9,10 +13,48 @@ import { AddEventPage } from '../add_event/add_event';
 })
 export class EventPage {
 
-  pushPage: any;
+  NewEvent: any;
+  EventInfo: any;
 
-  constructor(public navCtrl: NavController) {
-    this.pushPage = AddEventPage;
+  constructor(public navCtrl: NavController, public storage: Storage) {
+    this.NewEvent = AddEventPage;
+    this.EventInfo = EventDetailPage;
+  }
+
+  _clickMich(){}
+
+  clickMich(){
+    console.log("clickMich gedrückt ...")
+    let evnt1: EventData = new EventData(this.storage);
+    let evnt2: EventData = new EventData(this.storage);
+
+    let evntlist: EventList = new EventList(this.storage);
+    evntlist.testStorage(55);
+
+    this.storage.set('hanswurst', '6');
+    this.storage.get('hanswurst').then((vario) => {
+      console.log("Hier ist Vario: ", vario);
+    })
+
+    evnt1.setTitle( "Mein viertes Event" );
+    evnt1.setDescription( "Ich habe ein Event erstellt." );
+    evnt1.setAttendances( 444 );
+
+    evnt1.printList();
+    evnt1.test(120);
+
+    evnt1.add();
+
+    evnt1.printList();
+
+    evnt2.load( 0 );
+
+    evnt2.log();
+
+    console.log(evnt2.getID());
+    console.log(evnt2.getTitle());
+    console.log(evnt2.getDescription());
+    console.log(evnt2.getAttendances());
   }
 
 }
